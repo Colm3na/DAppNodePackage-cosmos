864df6777fdf5f8df2f157e80e61425b2f85e286
DerFredy - @4llfr33d0m:matrix.org <derfredy@gmail.com>
2019-06-26 19:55:25 +0200

    Got itgit add dappnode_package.json  syncing!

--- build/start_script.sh
+++ build/start_script.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 # Start the first process
+
+mkdir -p /root/.gaiad/config
+cp /genesis.json /root/.gaiad/config
+cp /config.toml /root/.gaiad/config
+
 tmux new-session -s "gaiad" -d -n "main"
 tmux send-keys -t "gaiad:main" C-z 'gaiad start' Enter
 tmux split-window -v
