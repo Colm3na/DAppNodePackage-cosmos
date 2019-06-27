564a73734c2c4715205743098d4910176bfcd4cb
DerFredy <derfredy@gmail.com>
2019-06-27 18:30:48 +0200

    New minimal version

--- build/start_script.sh
+++ build/start_script.sh
@@ -5,8 +5,11 @@ mkdir -p /root/.gaiad/config
 cp /genesis.json /root/.gaiad/config
 cp /config.toml /root/.gaiad/config
 
-tmux new-session -s "gaiad" -d -n "main"
-tmux send-keys -t "gaiad:main" C-z 'gaiad start' Enter
-tmux split-window -v
-tmux select-pane -t 1
-tmux send-keys "cd /usr/src/app/lunie && npm run serve" C-m
+nohup /usr/bin/gaiad start &
+nginx
+
+#tmux new-session -s "gaiad" -d -n "main"
+#tmux send-keys -t "gaiad:main" C-z 'gaiad start' Enter
+#tmux split-window -v
+#tmux select-pane -t 1
+#tmux send-keys "cd /usr/src/app/lunie && npm run serve" C-m
