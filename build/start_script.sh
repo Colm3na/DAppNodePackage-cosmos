8307f31b4e1eace1f435bdb0e00c45f82bef2774
DerFredy - @4llfr33d0m:matrix.org <derfredy@gmail.com>
2019-07-03 13:23:15 +0200

    Fighting with swagger

--- build/start_script.sh
+++ build/start_script.sh
@@ -15,10 +15,11 @@ fi
 
 nginx > /dev/null 2>&1 & 
 
-gaiad start \
-&& sleep 30
+gaiad start 
+
+# sleep 30 && \
 #gaiacli rest-server --chain-id=cosmoshub-2 \
 #    --laddr=tcp://localhost:1317 \
 #    --node tcp://localhost:26657 \
 #    --trust-node=false
-gaiacli rest-server --trust-node --cors * --laddr tcp://0.0.0.0:1317 &
+# gaiacli rest-server --trust-node --cors * --laddr tcp://0.0.0.0:1317 &
