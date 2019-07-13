6e4fd721e6c71ff33fefbc8635f182067cb0c890
DerFredy - @4llfr33d0m:matrix.org <derfredy@gmail.com>
2019-07-13 10:59:58 +0200

    primeros intentos

--- build/start_script.sh
+++ build/start_script.sh
@@ -13,6 +13,8 @@ then
   cp /usr/src/app/config.toml /root/.gaiad/config/config.toml
 fi
 
+cp /usr/src/app/*.pem /etc/nginx/certs
+
 nginx > /dev/null 2>&1 & 
 
 /bin/sh /usr/bin/start_rest.sh > /dev/null 2>&1 &
