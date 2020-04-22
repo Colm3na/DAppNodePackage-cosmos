b6eb2fee68a3cf8dec1eff8dc54aba2f219cc48a
DerFredy - @derfredy:matrix.org <derfredy@gmail.com>
2020-04-22 16:55:56 +0200

    Update gaia node

--- build/start_script.sh
+++ build/start_script.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 # Start the first process
 
+# Download genesis file one time
+if [ ! -f $HOME/.gaiad/config/genesis.json ]; then 
+    curl https://raw.githubusercontent.com/cosmos/launch/master/genesis.json > $HOME/.gaiad/config/genesis.json
+fi
+
+# Start the web server
 nginx > /dev/null 2>&1 & 
+
+# Start the rest server
 /bin/sh /usr/bin/start_rest.sh > /dev/null 2>&1 &
+
+# Start the tendermint node
 gaiad start
